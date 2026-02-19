e9669ff2dbde7358c1f34a593e8cd484ee516b21
Kun <monanzkun@gmail.com>
2026-02-19 17:30:22 +0100

    fixed validate method and added username to reservation serializer for visibility

--- reservation_system/reservation/serializers.py
+++ reservation_system/reservation/serializers.py
@@ -2,7 +2,8 @@ from .models import Table, Reservation
 
 from rest_framework.serializers import (
     ModelSerializer,
-    ValidationError
+    ValidationError,
+    StringRelatedField
 )
 
 
@@ -12,22 +13,25 @@ class TableSerializer(ModelSerializer):
         fields = ['id', 'number', 'seat', 'location']
         read_only_fields = ['id']
 
-        def validate(self, data):
-            seat = data.get('seat')
-            if seat == 0:
-                raise ValidationError(
-                    "Seat count must be greater than zero."
-                )
-            return data
+    def validate(self, attrs):
+        seat = attrs.get('seat')
+        if seat == 0:
+            raise ValidationError(
+                "Seat count must be greater than zero."
+            )
+        return attrs
 
 
 class ReservationSerializer(ModelSerializer):
+    username = StringRelatedField(read_only=True, source='user.username')
+
     class Meta:
         model = Reservation
         fields = [
             'id',
             'table',
             'user',
+            'username',
             'date',
             'time',
             'guests',
@@ -38,19 +42,20 @@ class ReservationSerializer(ModelSerializer):
             'id',
             'user',
             'created_at',
-            'status'
+            'status',
+            'username'
         ]
 
-        def validate(self, data):
-            table = data.get('table')
-            guests = data.get('guests')
+    def validate(self, attrs):
+        table = attrs.get('table')
+        guests = attrs.get('guests')
 
-            if guests == 0:
-                raise ValidationError(
-                    "Guest count must be greater than zero."
-                )
-            if guests > table.seat:
-                raise ValidationError(
-                    f"Guest count cannot exceed table seat count of {table.seat}."
-                )
-            return data
+        if guests == 0:
+            raise ValidationError(
+                "Guest count must be greater than zero."
+            )
+        if guests > table.seat:
+            raise ValidationError(
+                f"Guest count cannot exceed table seat count of {table.seat}."
+            )
+        return attrs
